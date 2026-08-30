2fa9f860a08d2672669314089a1870df270fb51f
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2026-08-30 20:34:46 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+
+        for (string& s : strs) {
+            vector<int> count(26, 0);
+
+            for (char c : s) {
+                count[c - 'a']++;
+            }
+
+            string key;
+            for (int x : count) {
+                key += "#" + to_string(x);
+            }
+
+            mp[key].push_back(s);
+        }
+
+        vector<vector<string>> ans;
+
+        for (auto& [key, group] : mp) {
+            ans.push_back(group);
+        }
+
+        return ans;
+    }
+};
