0f90e242d26df85206ed595db0453474276fd41e
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2026-09-24 18:58:20 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    void solve(vector<int>& nums,int i,int n,vector<int>&temp,vector<bool>&used){
+        if(temp.size() == n){ans.push_back(temp);return;}
+        if(i == n) return;
+        //pick
+        if(!used[i]){
+            used[i]=true;
+            temp.push_back(nums[i]);
+            solve(nums,0,n,temp,used);
+            //not pick
+            temp.pop_back();
+            used[i]=false;
+        }
+        solve(nums,i+1,n,temp,used);
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int>temp;
+        vector<bool>used(nums.size(),false);
+        solve(nums,0,nums.size(),temp,used);
+        return ans;
+        
+    }
+};
