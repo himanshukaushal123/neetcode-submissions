59e15aa49ee92f243b7e6516cec1e02e5662c8db
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2026-09-24 23:33:51 +0530

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string>ans;
+    void solve(int open , int close , string op){
+        if(open==0 && close==0){
+            ans.push_back(op);
+            return;
+        }
+        if(open){
+            op+="(";
+            solve(open-1,close,op);
+             op.pop_back(); 
+        }
+        if(close>open){
+            op+=")";
+            solve(open,close-1,op);
+            op.pop_back(); 
+        }
+        
+    }
+    vector<string> generateParenthesis(int n) {
+        solve(n ,n , "");
+        return ans;
+    }
+};
