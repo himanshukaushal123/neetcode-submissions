b4b42f328d3c513cbdd2247dc73aa5c633d15b4c
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2026-09-26 15:51:29 +0530

    Add: combination-target-sum - submission-5

---
+++ Data Structures & Algorithms/combination-target-sum/submission-5.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+// nums = [2,3,6,7]
+// target = 7
+//                          [] (target=7)
+ //                       /             \
+            //         TAKE 2           SKIP 2
+            //           [2]             []
+            //         /     \          /    \
+            //   TAKE 2     SKIP 2   TAKE 3  SKIP 3
+            //    [2,2]       [2]      [3]      []
+            //      |           |        |        |
+            //   TAKE 2       TAKE 3  TAKE 3    TAKE 6...
+            //   [2,2,2]      [2,3]    [3,3]
+            //      |           |        |
+            //   target 1     target 2  target 1
+            //      |           |        |
+            //    STOP        STOP      STOP
+    vector<vector<int>> ans;
+
+    void solve(vector<int> &nums,int n,int i,int target,vector<int>&temp){
+        if(target==0){
+            ans.push_back(temp);
+            return;
+        }
+        // pick
+        for(int j=i;j<=n-1;j++){
+            if(nums[j]>target)break;
+            temp.push_back(nums[j]);
+            solve(nums,n,j,target-nums[j],temp);
+             // Backtrack
+            temp.pop_back();
+        }
+
+    }
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<int>temp;
+        sort(nums.begin(),nums.end());
+        solve(nums,nums.size(),0,target,temp);
+        return ans;
+    }
+};
